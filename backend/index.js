35726012bc1559197b3f8895355fcd58c5f1f5b0
lakshmi <tulasiaudi1372@gmail.com>
2023-03-11 04:00:58 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -54,7 +54,7 @@ app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 5000
-const server = app.listen(5000, console.log(`server started in port ${PORT}`))
+const server = app.listen(PORT, console.log(`server started in port ${PORT}`))
 
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
