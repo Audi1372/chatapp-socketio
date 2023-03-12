4dcc04a05176721b39a84e345314471073b5c439
lakshmi <tulasiaudi1372@gmail.com>
2023-03-13 03:08:43 +0530

    added endpoint

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -15,7 +15,7 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModel from "./miscellanious/UpdateGroupChatModel";
 import { Chatstate } from "../Context/ChatProvider";
-const ENDPOINT = "https://chatapp-socketio2.vercel.app/";
+const ENDPOINT = "https://chatapp-socketio.vercel.app/";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
