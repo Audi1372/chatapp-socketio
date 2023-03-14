47d127c7d99432c1ea656bfdeee430277427f6e8
lakshmi <tulasiaudi1372@gmail.com>
2023-03-15 02:12:00 +0530

    i changed directory

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -15,7 +15,7 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModel from "./miscellanious/UpdateGroupChatModel";
 import { Chatstate } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://chat-api-zzkj.onrender.com";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
